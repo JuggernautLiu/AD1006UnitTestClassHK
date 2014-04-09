1716bc04976666fcc57874ceb3b640ea33d90258
JuggernautLiu <juggernaut.liu@gmail.com>
2014-04-09 16:44:57 +0800

    Add UnitTestHW3 and create release build

---
+++ CppCode/UT_Demo/UnitTestHW3/TEST_NonI_CCalculator.cpp
@@ -0,0 +1,10 @@
+#include "StdAfx.h"
+#include "TEST_NonI_CCalculator.h"
+
+TEST_NonI_CCalculator::TEST_NonI_CCalculator(void)
+{
+}
+
+TEST_NonI_CCalculator::~TEST_NonI_CCalculator(void)
+{
+}
---
+++ CppCode/UT_Demo/UnitTestHW3/TEST_NonI_CCalculator.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class TEST_NonI_CCalculator
+{
+public:
+    TEST_NonI_CCalculator(void);
+    virtual ~TEST_NonI_CCalculator(void);
+};
